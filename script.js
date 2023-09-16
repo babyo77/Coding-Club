caacb358a10d3c0345a14b60821594f71f56224a
babyo7 <144552425+babyo77@users.noreply.github.com>
2023-09-16 20:57:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -147,15 +147,7 @@ Level :  ${userskill}
        console.log("sent")
 
     })
-    onerror(error){
-        alert("Message sending failed. Please try again.");
-        load.style.display = "none"
-        header.style.visibility = "visible"
-        main.style.visibility = "visible"
-        load.style.display = "none";
-        whytojoin.style.display="block"
-        submit.innerHTML="Netwrok Error";
-    }
+    
 
 }
 
